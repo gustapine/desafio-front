0c4f5e44c52e1336c44f7cd9a6e861d5c8e894b3
gustavo dias <gustavohd34@gmail.com>
2023-07-25 13:02:32 -0300

    master

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Progress from './Progress'
+
+const Header = () => {
+  return (
+    <div>
+        <Progress/>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Progress.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Progress = (props) => {
+const { bgcolor, completed } = props;
+  return (
+    <div>
+    <div>
+      <span>oi</span>
+    </div>
+  </div>
+  )
+}
+
+export default Progress
