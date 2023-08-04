198f0bca1795b3c583ce63f9fff1fb9a52a78122
Gustavo Dias <gustavohd3b@gmail.com>
2023-08-03 22:29:09 -0300

    filter style

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Filter.css'
 
 const Filter = () => {
+  const [selecionado , setIsSelected] = useState(false);
+  const handleButtonClick = () => {
+    setIsSelected(!selecionado);
+  };
+
   return (
     <div className='Filter'>
-        <button className='Done'>Done</button>
-        <button className='Pending'>Pending</button>
+        <button className={`Done botao ${selecionado ? 'selecionado' : ''}`}
+        onClick={handleButtonClick} id='botao'>Done</button>
+        <button className={`Pending botao ${selecionado ? 'selecionado' : ''}`}
+        onClick={handleButtonClick}>Pending</button>
     </div>
   )
 }
