45ad086e52271c27abb5de5f0f0ce2abb856b97b
Gustavo Dias <gustavohd3b@gmail.com>
2023-08-20 14:54:31 -0300

    add btns de remove and add card

--- src/components/ItemToDo.jsx
+++ src/components/ItemToDo.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ItemToDo.css'
 
 const ItemToDo = (props) => {
+  const [edit, setEdit] = useState(false)
+  const [createElement, setElement] = useState(false)
+
+  const handleButtonClick = () => {
+    setEdit(!edit);
+  }
+
+  const createElemTooltip = () => {
+    setElement(!createElement)
+}
+
   return (
     <div>
-      <div className='itemCard'>
-      <p>{props.description}</p>
+      <div className={`itemCard ${edit ? 'edit' : ''}`} onClick={()=>{handleButtonClick(); createElemTooltip();}}>
+        <p>{props.description}</p>
+        <div className={`${!createElement && 'card-tooltip-none'} `} id={`${createElement ? 'card-tooltip' : ''} `}><span id={`${createElement ? 'card-tooltip' : ''}`}>Edit task</span></div>
+        <div className={`${edit ? 'buttonsCard' : 'buttonsCardDisplay' }`}>
+          <button className={`${edit ? 'removeButton' : 'buttonsCardDisplay' }`}></button>
+          <button className={`${edit ? 'addCardButton' : 'buttonsCardDisplay' }`}></button>
+        </div>
       </div>
     </div>
   )
--- src/itemsToDo.js
+++ src/itemsToDo.js
@@ -1,23 +1,28 @@
 const itemsToDo =[
     {
         key: 1,
-        description: 'Drive a Cadillac across the Irish Sea'
+        description: 'Drive a Cadillac across the Irish Sea',
+        isCompleted: false
     },
     {
         key: 2,
-        description: 'Sell an elevator to Geronimo'
+        description: 'Sell an elevator to Geronimo',
+        isCompleted: false
     },
     {
         key: 3,
-        description: 'Take a pound of love and cook it in the stew'
+        description: 'Take a pound of love and cook it in the stew',
+        isCompleted: false
     },
     {
         key: 4,
-        description: "Dont't let them tell you what you wanna be"
+        description: "Dont't let them tell you what you wanna be",
+        isCompleted: false
     },
     {
         key: 5,
-        description: 'Do a great job and join the best Product team...'
+        description: 'Do a great job and join the best Product team...',
+        isCompleted: false
     }
 ];
 
